cf67e816b02b4ade0534c06f233d2a229bc6cf03
Denis Drabinka <denisdrabinka@mail.ru>
2019-04-17 17:19:02 +0200

    Building the Basic UI Layer

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -8,5 +8,12 @@ namespace FriendOrganizer.Model
 {
     public class Friend
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
     }
 }
