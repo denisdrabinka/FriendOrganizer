3fcf01082aab9c60a0cf35300ba283c0ff0dc165
Denis Drabinka <denisdrabinka@mail.ru>
2019-04-19 11:04:53 +0200

    05. Setting up Entity Framework
    05. Add Constraints

---
+++ FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,28 @@
+namespace FriendOrganizer.DataAccess.Migrations
+{
+    using Model;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FriendOrganizer.DataAccess.FriendOrganizerDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FriendOrganizer.DataAccess.FriendOrganizerDbContext context)
+        {
+            context.Friends.AddOrUpdate(
+       f => f.FirstName,
+       new Friend { FirstName = "Thomas", LastName = "Huber" },
+       new Friend { FirstName = "Urs", LastName = "Meier" },
+       new Friend { FirstName = "Erkan", LastName = "Egin" },
+       new Friend { FirstName = "Sara", LastName = "Huber" }
+       );
+
+        }
+    }
+}
